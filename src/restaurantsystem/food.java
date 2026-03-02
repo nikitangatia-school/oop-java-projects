f832b9817df6f55ea3dcdd1e8d706baac753e5a8
nikitangatia-school <nikita.ngatia@strathmore.edu>
2026-03-02 08:48:29 +0300

    Create food.java

---
+++ src/restaurantsystem/food.java
@@ -0,0 +1,14 @@
+abstract class Food {
+    private String name;
+    private double price;
+    
+    public Food(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+    
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+    
+    public abstract void make();
+  }
